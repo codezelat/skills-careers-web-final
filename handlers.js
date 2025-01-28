869771a6c4bd780379520322ca912a30f5dd32e0
Miyuri15 <miyurilokuhewage15@gmail.com>
2025-01-29 05:17:31 +0530

    press release done

--- handlers.js
+++ handlers.js
@@ -6,3 +6,10 @@ export const handleOpenForm = (isFormVisible) => () => {
     isFormVisible(false);
   };
 
+  export function formatDate(dateString) {
+    if (!dateString) return "Unknown date";
+    const options = { year: "numeric", month: "long", day: "numeric" };
+    const date = new Date(dateString);
+    return date.toLocaleDateString(undefined, options);
+  }
+  
