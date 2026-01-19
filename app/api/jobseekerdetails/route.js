292218e37990e4562351065eb2befc99df20cb0c
Sayuru Akash Amarasinghe <sayuru555@gmail.com>
2026-01-20 02:20:43 +0530

    Fix jobseeker DOB initialization

--- app/api/jobseekerdetails/route.js
+++ app/api/jobseekerdetails/route.js
@@ -69,7 +69,7 @@ export async function POST(req) {
       bio: "",
       city: "",
       country: "",
-      dateOfBirth: null,
+      dob: null,
       gender: "",
       skills: [],
       softSkills: [],
